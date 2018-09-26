af64ccd6f1f6540e6fde86f34ee824c19bb8283f
Anthony G Micodin <amicodin@o244-20.csuchico.edu>
2018-09-26 16:37:55 -0700

    Added the do while loop and the numitems tracker. Program complete.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+// Author: Anthony Micodin
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,35 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  string item;
   cout<<"\n==GROCERY LIST MANAGER==";
+  
+  
+  do 
+  {
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
 
+   if ( input == 'a' || input == 'A')
+   {
+     cout<<"What is the item?"<<endl;
+     cin>>item;
+      if ( numItems <  5 )
+      {
+              list[numItems] = item;
+              numItems ++;
+      }
+      else 
+      {
+              cout<<"You'll need a bigger list!"<<endl;
+      }
+   }
+   else
+     {}
+  }
+  while( input != 'Q' &&  input != 'q');
   return 0;
 }
